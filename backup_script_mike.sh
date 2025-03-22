40dd83dcfdd45dcf1c14c712f90bb842a88a96c8
Avinjay Mewada WSL <avinjay@avinjaymewada25@gmail.com>
2025-03-23 01:24:58 +0530

    Backup script of nginx for Mike added

---
+++ backup_script_mike.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Backup script for Apache and Nginx servers on Debian/Ubuntu
+
+# Define variables
+date=`date +"%Y-%m-%d-%H-%M-%S"`
+nginx_backup_dir="/home/mike/backups/nginx_backup_$date.tar.gz"
+log_file="/home/mike/logs/backup_"$date".log"
+
+tar -czf "$nginx_backup_dir" /etc/nginx/ /usr/share/nginx/html/
+
+# Verify backups and log results
+echo "Backup Verification - $date" > "$log_file"
+if tar -tzf "$nginx_backup_dir" &>/dev/null; then
+    echo "Nginx backup ($nginx_backup_dir) is valid." >> "$log_file"
+else
+    echo "Nginx backup ($nginx_backup_dir) is corrupted!" >> "$log_file"
+fi
+
+# Display log file
+tail -n 100 "$log_file"
